917e498ad6715a621c17dd2dc47ec94554804711
ryan jin <ryanjin333@gmail.com>
2023-09-15 23:48:48 -0400

    first commit

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const port = 4040;
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(port, () => console.log(`Server is running on port: ${port}`));
+    app.listen(port, () => console.log(`Server is running on  port: ${port}`));
   } catch (error) {
     console.log(error); 
   }
